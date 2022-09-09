a8342d8da9af9c8a256b5bb5f8f1f8a073129f98
WanguiEss <ndirangulynette00@gmail.com>
2022-09-09 00:15:27 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Printing
+ * 'main' - Printing
  *
  * Description: using the main function
  * this program prints  "Programming is like building a multilingual puzzle
